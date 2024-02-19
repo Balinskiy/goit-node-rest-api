cfc22b7e0a40a483e17aa9f37ba02e2a3313acc6
Balinskiy <3879342@gmail.com>
2024-02-19 18:58:45 +0200

    upd-hw2-express

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,11 +1,6 @@
 import express from "express";
-import {
-  getAllContacts,
-  getOneContact,
-  deleteContact,
-  createContact,
-  updateContact,
-} from "../controllers/contactsControllers.js";
+import { getAllContacts, getOneContact, deleteContact, createContact, updateContact } from "../controllers/contactsControllers.js";
+import validateBody from "../helpers/validateBody.js";
 
 const contactsRouter = express.Router();
 
@@ -15,8 +10,8 @@ contactsRouter.get("/:id", getOneContact);
 
 contactsRouter.delete("/:id", deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post("/", validateBody, createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put("/:id", validateBody, updateContact);
 
 export default contactsRouter;

04db38f83edadf0e659612364cc8ca34be1a8e86
Balinskiy <3879342@gmail.com>
2024-03-09 13:01:06 +0200

    upd-hw4-final-v.3

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
 import authController from "../controllers/authController.js";
 import authenticate from "../middlewares/authenticate.js";
+import isValidBody from "../middlewares/isValidBody.js";
+import validateBody from "../helpers/validateBody.js";
+import { userLoginSchema, userRegisterSchema } from "../schemas/userSchema.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", authController.register);
-authRouter.post("/login", authController.login);
+authRouter.post("/register", isValidBody, validateBody(userRegisterSchema), authController.register);
+authRouter.post("/login", isValidBody, validateBody(userLoginSchema), authController.login);
 authRouter.post("/logout", authenticate, authController.logout);
 authRouter.get("/current", authenticate, authController.getCurrent);
 

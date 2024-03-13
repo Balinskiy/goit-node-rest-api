d569593cc4e3488ba3054c3045f6cec03c40fe60
Balinskiy <3879342@gmail.com>
2024-03-13 12:07:45 +0200

    upd-hw5-avatarUrl

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,14 +5,15 @@ import isValidBody from "../middlewares/isValidBody.js";
 import validateBody from "../helpers/validateBody.js";
 import { userLoginSchema, userRegisterSchema } from "../schemas/userSchema.js";
 import upload from "../middlewares/upload.js";
+import isValidId from "../middlewares/isValidId.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", isValidBody, validateBody(userRegisterSchema), authController.register);
 authRouter.post("/login", isValidBody, validateBody(userLoginSchema), authController.login);
-authRouter.post("/logout", authenticate, authController.logout);
-authRouter.get("/current", authenticate, authController.getCurrent);
-authRouter.get("/avatar", authenticate, authController.getAvatar);
-authRouter.patch("/avatar", authenticate, upload.single("avatarURL"), authController.updateAvatar);
+authRouter.post("/logout", authenticate, isValidId, authController.logout);
+authRouter.get("/current", authenticate, isValidId, authController.getCurrent);
+authRouter.get("/avatars", authenticate, authController.getAvatar);
+authRouter.patch("/avatars", authenticate, upload.single("avatarURL"), authController.updateAvatar);
 
 export default authRouter;
